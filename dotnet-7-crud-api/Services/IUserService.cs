802246f86a62a85cffecd2938021ccd5736a1f33
Isaiah Clifford opoku <opokuisaiahclifford123@gmail.com>
2024-01-28 14:16:49 +0000

    Adding of user services

---
+++ dotnet-7-crud-api/Services/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using dotnet_7_crud_api.Entitiles;
+using dotnet_7_crud_api.Models.Users;
+using System;
+
+namespace dotnet_7_crud_api.Services
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<User>> GetAll();
+        Task<User> GetById(Guid id);
+        Task Create(CreateRequest model);
+        Task Update(Guid id, UpdateRequest model);
+        Task Delete(Guid id);
+    }
+}
